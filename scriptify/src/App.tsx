e2bc1f0156b4051b2d61943ab5c21bd00bdedb55
Nick Kravchuk <kravchuknick@gmail.com>
2024-04-02 12:30:51 -0700

    added button, card, input, label, & tabs custom components - edited the log in form

---
+++ scriptify/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from "./components/ui/button"
+import  LoginForm from "./components/login/LoginForm.tsx"
+
+
+function App() {
+
+  return (
+    <div>
+      <LoginForm/>
+      <Button>Test</Button>
+    </div>
+  )
+}
+
+export default App
