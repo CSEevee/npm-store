0a62491c956ff9032918a61b6be3b6a92dc0665f
Nick Kravchuk <kravchuknick@gmail.com>
2024-04-02 17:08:44 -0700

    added data table component from shadcn Co-authored-by: Matt Konop <mattsterprogrammer@gmail.com> Co-authored-by: Allison Scott <allisonbscott97@gmail.com> Co-authored-by: Nick Kravchuk <kravchuknick@gmail.com> Co-authored-by: Victoria Obregon <victoria.m.obregon@gmail.com>

--- scriptify/src/App.tsx
+++ scriptify/src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+// import React from 'react';
 import { Button } from "./components/ui/button"
-import  LoginForm from "./components/login/LoginForm.tsx"
-
+// import  LoginForm from "./components/login/LoginForm.tsx"
+// import Dashboard  from './components/home/home.tsx';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { DataTableDemo } from "./components/home/home.tsx";
 
 function App() {
 
   return (
+  <Router>
     <div>
-      <LoginForm/>
+      <DataTableDemo/>
       <Button>Test</Button>
     </div>
+    </Router>
   )
 }
 
