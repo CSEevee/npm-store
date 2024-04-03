5f235e9cce245d55a61bd827f10c7bd474158bc0
Allison Scott <allisonbscott97@gmail.com>
2024-04-02 22:10:51 -0500

    set up server file, ts configuration, routes, middleware, CRU functionality, connected to supabase/postgres db
    Co-authored-by: Matt Konop <mattsterprogrammer@gmail.com>
    Co-authored-by: Allison Scott <allisonbscott97@gmail.com>
    Co-authored-by: Nick Kravchuk <kravchuknick@gmail.com>
    Co-authored-by: Victoria Obregon <victoria.m.obregon@gmail.com>

--- scriptify/src/server/server.ts
+++ scriptify/src/server/server.ts
@@ -1,37 +1,93 @@
-import express from 'express';
-import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv';
+import express, { Request, Response, NextFunction} from 'express';
+import { supabase } from './superbaseClient';
+import { SQLController } from './sqlController';
+import { userController } from './userController';
 
-
-//supabase connection
-const supabaseUrl = 'https://wixzydeumdmhubkemfgy.supabase.co'
-dotenv.config();
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-supabase
-  .from('userprofiles')
-  .select('*')
-  .then(({ data, error }) => {
-    if (error) {
-      console.error('Error connecting to Supabase:', error)
-    } else {
-      console.log('Successfully connected to Supabase!')
-      // You can now perform additional operations with Supabase
-    }
-  })
+  const retrievedData = async() => {
+    const { data, error } = await supabase.from('userprofiles').select('*');
+    if(!error) console.log('data accessed',data);
+  }
+retrievedData();
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 3001; // Ensure this port does not conflict with Vite's
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+//delcaring a type for ServerError object
+type ServerError = {
+  log: string;
+  status: number;
+  message: {err: string};
+}
 
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, world!');
+  });
+  
+  
+  //route - get requests to Supabase (WORKING)
+  app.get('/database', (req: Request,res: Response) => {
+    res.status(200).send('database retrieved');
+  });
 
+  //route - get favorites for a user (WORKING)
+  app.get('/database/favorites', SQLController.getFavorites, userController.verifyCookie, (req: Request,res: Response) => {
+    res.status(200).json(res.locals.favorites);
+  });
+  
+  // route - get previous checkouts (WORKING)
+  app.get('/database/previouscheckout', SQLController.getPreviousCheckout, userController.verifyCookie, (req: Request,res: Response) => {
+    res.status(200).json(res.locals.prevCheckouts);
+  });
 
+  //route- add favorite to Supabase (WORKING)
+  app.put('/database/favorites', SQLController.addFavorites, userController.verifyCookie, (req: Request,res: Response) => {
+    res.status(200).send('favorites added');
+  });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+  //route - save a user's past checkouts in Supabase (WORKING)
+  app.put('/database/previouscheckout', SQLController.addPreviousCheckout, userController.verifyCookie, (req: Request,res: Response) => {
+    res.status(200).send('Checkout added');
+  });
+
+  //stretch feature:
+  // app.delete('/database/favorites', SQLController.deleteFavorites, (req: Request,res: Response) => {
+  //   res.status(200).send('entry/entries deleted');
+  // });
+
+  // app.delete('/database/previouscheckout', SQLController.deletePreviousCheckout, (req: Request,res: Response) => {
+  //   res.status(200).send('entry/entries deleted');
+  // });
+
+  //route - user signup (WORKING)
+  app.post('/signup', userController.createUser, userController.issueCookie, userController.verifyCookie, (req: Request, res: Response) => {
+    res.status(200).send('new user is added');
+  });
+
+  //route- user login (WORKING)
+  app.post('/login', userController.getUserProfile, userController.issueCookie, userController.verifyCookie, (req, res) => {
+    res.status(200).send('user signed in');
+  });
+  
+  //catchall for non-defined pages
+  app.use('*', (req: Request, res: Response) => {
+    return res.status(404).send('Page not found.')
+  });
+  
+  //global error handler
+  app.use((err: ServerError, req: Request, res: Response, next: NextFunction) => {
+    const defaultErr: ServerError = {
+      log: `Express error handler caught unknown middleware error ${err}`,
+      status: 500,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj: ServerError = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  })
+  
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
