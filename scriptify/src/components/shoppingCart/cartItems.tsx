f822ceaa6e41f4299f0ece8d19e10ad972c8738e
tori-obregon <vobregon@gmail.com>
2024-04-03 13:42:30 -0700

    cartItems.tsx and npmItem.tsx is dynamic with props

--- scriptify/src/components/shoppingCart/cartItems.tsx
+++ scriptify/src/components/shoppingCart/cartItems.tsx
@@ -1,26 +1,26 @@
 import {NpmItem} from './npmItem';
 
 const testPackages = [
-    { _id: '1', name: 'supertest', version: '6.3.4' , time: '2024-01-14T16:41:50.357Z', repository: 'github.com/ladjs/supertest'},
-    { _id: '2', name: 'superagent', version: '8.1.2' , time: '2023-08-15T22:34:23.172Z', repository: 'github.com/ladjs/superagent'},
-    { _id: '3', name: 'lodash', version: '4.17.21' , time: '2024-01-14T16:41:50.357Z', repository: 'github.com/lodash/lodash'},
-    { _id: '4', name: 'react', version: '18.2.0' , time: '2024-01-14T16:41:50.357Z', repository: 'github.com/facebook/react'},
-    { _id: '5', name: 'axios', version: '1.6.8' , time: '2024-01-14T16:41:50.357Z', repository: 'github.com/axios/axios'},
-    { _id: '6', name: 'tslib', version: '2.6.2' , time: '2024-01-14T16:41:50.357Z', repository: 'github.com/Microsoft/tslib'},
-    { _id: '7', name: 'chalk', version: '5.3.0' , time: '2024-01-14T16:41:50.357Z', repository: 'github.com/chalk/chalk'},
+    { _id: '1', name: 'supertest', version: '6.3.4' , date: '2024-01-14T16:41:50.357Z', repository: 'github.com/ladjs/supertest'},
+    { _id: '2', name: 'superagent', version: '8.1.2' , date: '2023-08-15T22:34:23.172Z', repository: 'github.com/ladjs/superagent'},
+    { _id: '3', name: 'lodash', version: '4.17.21' , date: '2021-02-20T15:42:16.891Z', repository: 'github.com/lodash/lodash'},
+    { _id: '4', name: 'react', version: '18.2.0' , date: '2022-06-14T19:46:38.369Z', repository: 'github.com/facebook/react'},
+    { _id: '5', name: 'axios', version: '1.6.8' , date: '2024-03-15T16:32:47.800Z', repository: 'github.com/axios/axios'},
+    { _id: '6', name: 'tslib', version: '2.6.2' , date: '2023-08-18T17:41:58.755Z', repository: 'github.com/Microsoft/tslib'},
+    { _id: '7', name: 'chalk', version: '5.3.0' , date: '2023-06-29T10:58:11.887Z', repository: 'github.com/chalk/chalk'},
     ]
    
 const CartItems = () => {
     return (
     <div className="container mx-auto px-8 py-12">
-        <h1 className='text-3xl font-bold mb-4'>Cart Items</h1>
+        <h1 className='text-3xl font-bold mb-4'>Your Packages</h1>
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
         {testPackages.map((npm) => {
             return (<NpmItem 
                 id={npm._id} 
                 name={npm.name}
                 version={npm.version}
-                time={npm.time}
+                date={npm.date}
                 repository={npm.repository}
                 />)
         })}
--- scriptify/src/components/shoppingCart/shoppingCart.tsx
+++ scriptify/src/components/shoppingCart/shoppingCart.tsx
@@ -4,21 +4,14 @@ import Checkout from './checkout/checkout';
 
 const ShoppingCart = () => {
   return (
-    <div>
-      <h2>Shopping Cart</h2>
+    <div className="container mx-auto px-8 py-12">
+      <h2 className='text-5xl font-bold mb-4'>Shopping Cart</h2>
       {/* Component 1: list of all packages in cart */}
       <CartItems/>
-      <ul>
-        {/* map over the list of packages in the user's shopping cart and display */}
-        {/* {packages.map((item, index) => (
-          <li key={index}>{item.name}</li>
-        ))} */}
-      </ul>
       {/* Component 2: generate NPM script and button to save to db 
       - include a copy button
       */}
       <Checkout />
-
     </div>
   );
 };
